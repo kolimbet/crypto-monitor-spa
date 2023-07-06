a90c2dd54f9a81739ca521f7e2f0ccbe224d3ac7
ДК <kolimbet-dv@yandex.ru>
2023-07-06 15:06:04 +0300

    Added output of a list of tickers with pagination and filtering

---
+++ src/components/TickerItem.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="col">
+    <div
+      class="position-relative overflow-hidden shadow rounded-3 border border-2 border-opacity-50 bg-white"
+    >
+      <!-- вывод ошибок API -->
+      <div class="position-absolute top-0 end-0 start-0">
+        <div class="position-relative">
+          <div class="px-2 py-1 text-end">
+            <span
+              class="d-inline-block p-1 bg-secondary text-white rounded fs-xs cursor-pointer"
+              title="Connection status indicator"
+              >Status</span
+            >
+            <!-- <span
+              class="d-inline-block p-1 bg-success text-white rounded fs-xs cursor-pointer"
+              title="You`re subscribed to this trading pair by WebSocket"
+              >WS</span
+            > -->
+            <!-- <span class="lh-1 fs-5 link-danger">
+              <i
+                class="fa fa-exclamation-circle cursor-pointer"
+                aria-hidden="true"
+                title="Show Errors"
+              ></i>
+            </span> -->
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="px-3 py-4 text-center border-bottom border-secondary border-opacity-50"
+      >
+        <dt class="fs-sm text-secondary text-truncate">
+          {{ ticker.name }} - {{ $store.state.ticker.currentCurrency }}
+        </dt>
+        <dd class="mt-1 mb-0 fs-2 fw-bold">
+          {{ ticker.price === null ? "-" : ticker.price }}
+        </dd>
+      </div>
+
+      <div class="d-grid">
+        <button
+          class="btn btn-gray-200 link-secondary mx-1 mb-1 p-3 d-flex align-items-center justify-content-center rounded-top-0"
+          title="Double click to delete the ticker"
+        >
+          <svg
+            class="h-5 w-5 me-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="#718096"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="fw-bold">Delete</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TickerItem",
+  props: {
+    ticker: Object,
+  },
+  emits: ["delete", "select"],
+  computed: {},
+  methods: {},
+};
+</script>
--- src/components/TickerList.vue
+++ src/components/TickerList.vue
@@ -9,6 +9,7 @@
             >Filter:</label
           >
           <input
+            v-model.trim="tickersFilter"
             id="tickerFilter"
             type="text"
             size="4"
@@ -16,21 +17,30 @@
           />
         </div>
         <div class="float-end mb-2">
-          <button
-            type="button"
-            class="btn btn-sm btn-secondary py-1 px-3 rounded-pill"
-          >
-            Back
-          </button>
+          <Transition name="fade">
+            <button
+              v-if="page > 1"
+              @click="goPrevPage()"
+              type="button"
+              class="btn btn-sm btn-secondary py-1 px-3 rounded-pill"
+            >
+              Back
+            </button>
+          </Transition>
+
           <label class="py-1 px-3 text-gray-700"
-            >page: <strong>2</strong> / 3</label
+            >page: <strong>{{ page }}</strong> / {{ totalPages }}</label
           >
-          <button
-            type="button"
-            class="btn btn-sm btn-secondary py-1 px-3 rounded-pill"
-          >
-            Next
-          </button>
+          <Transition name="fade">
+            <button
+              v-if="hasNextPage"
+              @click="goNextPage()"
+              type="button"
+              class="btn btn-sm btn-secondary py-1 px-3 rounded-pill"
+            >
+              Next
+            </button>
+          </Transition>
         </div>
       </div>
       <hr class="mt-2 w-percent-100 border-secondary" />
@@ -47,113 +57,108 @@
       </div>
     </Transition>
 
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      <template v-for="index in 3" :key="index">
-        <div class="col">
-          <div
-            class="position-relative overflow-hidden shadow rounded-3 border border-2 border-opacity-50 bg-white"
-          >
-            <div class="position-absolute top-0 end-0 start-0">
-              <div class="position-relative">
-                <div class="px-2 py-1 text-end">
-                  <span
-                    class="d-inline-block p-1 bg-success text-white rounded fs-xs cursor-pointer"
-                    title="You`re subscribed to this trading pair by WebSocket"
-                    >WS</span
-                  >
-                </div>
+    <Transition name="folding-y-1000" mode="out-in">
+      <template v-if="hasFilteredTickers">
+        <TransitionGroup
+          name="list-folding-y-200"
+          tag="div"
+          class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+        >
+          <TickerItem v-for="t in paginatedTickers" :key="t.id" :ticker="t" />
+        </TransitionGroup>
+      </template>
+      <template v-else>
+        <div class="row">
+          <div class="col text-center">
+            <template v-if="hasTickers">
+              <div class="mb-2 fs-5">
+                None of the tickers matches the installed filter
               </div>
-            </div>
-
-            <div
-              class="px-3 py-4 text-center border-bottom border-secondary border-opacity-50"
-            >
-              <dt class="fs-sm text-secondary text-truncate">BTC - USD</dt>
-              <dd class="mt-1 mb-0 fs-2 fw-bold">30702.27</dd>
-            </div>
-
-            <div class="d-grid">
-              <button
-                class="btn btn-gray-200 link-secondary mx-1 mb-1 p-3 d-flex align-items-center justify-content-center rounded-top-0"
-                title="Double click to delete the ticker"
-              >
-                <svg
-                  class="h-5 w-5 me-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="#718096"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span class="fw-bold">Удалить</span>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div
-            class="position-relative overflow-hidden shadow rounded-3 border border-2 border-opacity-50 bg-danger bg-opacity-25"
-          >
-            <div class="position-absolute top-0 end-0 start-0">
-              <div class="position-relative">
-                <div class="px-2 py-1 text-end">
-                  <span class="lh-1 fs-5 link-danger">
-                    <i
-                      class="fa fa-exclamation-circle cursor-pointer"
-                      aria-hidden="true"
-                      title="Show Errors"
-                    ></i>
-                  </span>
-                </div>
+            </template>
+            <template v-else>
+              <div class="mb-2 fs-5">Not a single ticker has been added.</div>
+              <div class="mb-2 text-sm text-secondary">
+                Use the widget at the top of the page to create a new ticker to
+                track the rate of the cryptocurrency you are interested in
               </div>
-            </div>
-
-            <div
-              class="px-3 py-4 text-center border-bottom border-secondary border-opacity-50"
-            >
-              <dt class="fs-sm text-secondary text-truncate">ETHD - USD</dt>
-              <dd class="mt-1 mb-0 fs-2 fw-bold">1.00</dd>
-            </div>
-
-            <div class="d-grid">
-              <button
-                class="btn btn-gray-200 link-secondary mx-1 mb-1 p-3 d-flex align-items-center justify-content-center rounded-top-0"
-                title="Double click to delete the ticker"
-              >
-                <svg
-                  class="h-5 w-5 me-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="#718096"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span class="fw-bold">Удалить</span>
-              </button>
-            </div>
+            </template>
           </div>
         </div>
       </template>
-    </div>
+    </Transition>
   </div>
 </template>
 
 <script>
 import ErrorMessage from "./ErrorMessage.vue";
+import TickerItem from "./TickerItem.vue";
 
 export default {
   name: "TickerList",
-  components: { ErrorMessage },
+  components: { ErrorMessage, TickerItem },
+  data() {
+    return {
+      page: 1,
+      limitTickersOnPage: 6,
+      tickersFilter: "",
+    };
+  },
+  computed: {
+    filteredTickers() {
+      let filter = this.tickersFilter.toUpperCase();
+
+      if (filter.length)
+        return this.$store.state.ticker.tickerList.filter(
+          (t) =>
+            t.name.includes(filter) ||
+            t.symbol.includes(filter) ||
+            t.fullNameUp.includes(filter)
+        );
+      else return this.$store.state.ticker.tickerList;
+    },
+    hasTickers() {
+      return this.$store.state.ticker.tickerList?.length;
+    },
+    hasFilteredTickers() {
+      return this.filteredTickers?.length;
+    },
+    totalPages() {
+      return this.hasFilteredTickers
+        ? Math.ceil(this.filteredTickers.length / this.limitTickersOnPage)
+        : 1;
+    },
+    hasNextPage() {
+      return this.totalPages > this.page;
+    },
+    paginatedTickers() {
+      const start = (this.page - 1) * this.limitTickersOnPage;
+      const end = this.page * this.limitTickersOnPage;
+      return this.filteredTickers.slice(start, end);
+    },
+  },
+  methods: {
+    goNextPage() {
+      if (this.hasNextPage) this.page += 1;
+    },
+    goPrevPage() {
+      if (this.page > 1) this.page -= 1;
+    },
+    resetPage() {
+      this.page = 1;
+    },
+    fixPageAverage() {
+      if (this.page > this.totalPages || this.page < 1) this.resetPage();
+    },
+  },
+  watch: {
+    paginatedTickers() {
+      if (this.page > 1 && this.paginatedTickers.length === 0)
+        this.goPrevPage();
+    },
+    tickersFilter() {
+      this.resetPage();
+    },
+  },
   async created() {
     this.$store.dispatch("initApp");
   },
